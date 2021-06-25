d5da3839358abe93d85979ad53f1fbbb6690d22d
GiovanniHM <giovannihernandezm26@gmail.com>
2021-06-25 10:49:36 -0500

    Class 331

--- GraphicsAngular/src/app/graficas/pages/barras/barras.component.ts
+++ GraphicsAngular/src/app/graficas/pages/barras/barras.component.ts
@@ -26,8 +26,9 @@ export class BarrasComponent implements OnInit {
   public barChartLegend = true;
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: "#F2BB30", hoverBackgroundColor: "red"  },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: "#FCAF32", hoverBackgroundColor: "red" },
+    { data: [8, 50, 20, 12, 85, 55, 71], label: 'Series C' , backgroundColor: "#E68D39", hoverBackgroundColor: "red" }
   ];
 
   constructor() { }
@@ -45,6 +46,29 @@ export class BarrasComponent implements OnInit {
       56,
       (Math.random() * 100),
       40 ];
+
+      this.barChartData[1].data = [
+        Math.round(Math.random() * 100),
+        59,
+        80,
+        (Math.random() * 100),
+        56,
+        (Math.random() * 100),
+        40 ];
+
+        this.barChartData[2].data = [
+          Math.round( Math.random() * 100 ),
+          Math.round( Math.random() * 100 ),
+          Math.round( Math.random() * 100 ),
+          Math.round( Math.random() * 100 ),
+          Math.round( Math.random() * 100 ),
+          Math.round( Math.random() * 100 ),
+          Math.round( Math.random() * 100 ),
+          Math.round( Math.random() * 100 )
+        ];
+
+
   }
+
 
 }
