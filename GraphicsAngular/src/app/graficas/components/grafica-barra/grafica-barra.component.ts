f73805c4e2f2c41dd1237d9ea508f613802881c7
GiovanniHM <giovannihernandezm26@gmail.com>
2021-06-25 10:59:58 -0500

    Class 332

--- GraphicsAngular/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ GraphicsAngular/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -6,6 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grafica-barra.component.css']
 })
 export class GraficaBarraComponent implements OnInit {
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    }
+  };
+  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: "#F2BB30", hoverBackgroundColor: "red"  },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: "#FCAF32", hoverBackgroundColor: "red" },
+    { data: [8, 50, 20, 12, 85, 55, 71], label: 'Series C' , backgroundColor: "#E68D39", hoverBackgroundColor: "red" }
+  ];
 
   constructor() { }
 
